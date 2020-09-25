8d20ca27fd99cfc8148a299da2e50b47207fdf66
sformichella <sformichella@gmail.com>
2020-09-25 07:42:41 -0700

    add two tests for each operation in mathUtils.js

--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,14 @@
 // IMPORT MODULES under test here:
-// import { example } from '../example.js';
+import{add, subtract, multiply, divide} from './mathUtils.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
-    const expected = true;
-    
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const actual = false;
+test('Expect 5 and 6 to return 11', (expect) => {
+    let firstNumber = 5;
+    let secondNumber = 6;
+    let expectedSum = 11;
 
-    //Expect
-    // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    let actualSum = add(firstNumber, secondNumber);
+
+    expect.equal(actualSum, expectedSum);
 });
--- test/tests.js
+++ test/tests.js
@@ -1,1 +1,136 @@
-import './example.test.js';
+import{add, subtract, multiply, divide} from '../mathUtils.js';
+
+
+
+    // add() Tests
+
+const addTestOne = QUnit.test;
+
+addTestOne('Expect 5 and 6 to return 11', (expect) => {
+    let firstNumber = 5;
+    let secondNumber = 6;
+    let expectedSum = 11;
+
+    let actualSum = add(firstNumber, secondNumber);
+
+    expect.equal(actualSum, expectedSum);
+});
+
+
+
+
+const addTestTwo = QUnit.test;
+
+addTestTwo('Expect 7 and 1 to return 8', (expect) => {
+    let firstNumber = 7;
+    let secondNumber = 1;
+    let expectedSum = 8;
+
+    let actualSum = add(firstNumber, secondNumber);
+
+    expect.equal(actualSum, expectedSum);
+});
+
+
+
+
+
+
+    // subtract() Tests
+
+const subtractTestOne = QUnit.test;
+
+subtractTestOne('Expect 5 and 6 to return 11', (expect) => {
+    let firstNumber = 5;
+    let secondNumber = 6;
+    let expectedSum = -1;
+
+    let actualSum = subtract(firstNumber, secondNumber);
+
+    expect.equal(actualSum, expectedSum);
+});
+
+
+
+
+const subtractTestTwo = QUnit.test;
+
+subtractTestTwo('Expect 7 and 1 to return 8', (expect) => {
+    let firstNumber = 7;
+    let secondNumber = 1;
+    let expectedSum = 6;
+
+    let actualSum = subtract(firstNumber, secondNumber);
+
+    expect.equal(actualSum, expectedSum);
+});
+
+
+
+
+
+
+    // multiply() Tests
+
+const multiplyTestOne = QUnit.test;
+
+multiplyTestOne('Expect 5 and 6 to return 11', (expect) => {
+    let firstNumber = 5;
+    let secondNumber = 6;
+    let expectedSum = 30;
+
+    let actualSum = multiply(firstNumber, secondNumber);
+
+    expect.equal(actualSum, expectedSum);
+});
+
+
+
+
+const multiplyTestTwo = QUnit.test;
+
+multiplyTestTwo('Expect 7 and 1 to return 8', (expect) => {
+    let firstNumber = 7;
+    let secondNumber = 1;
+    let expectedSum = 7;
+
+    let actualSum = multiply(firstNumber, secondNumber);
+
+    expect.equal(actualSum, expectedSum);
+});
+
+
+
+
+
+    // divide() Tests
+
+const divideTestOne = QUnit.test;
+
+divideTestOne('Expect 5 and 6 to return 11', (expect) => {
+    let firstNumber = 20;
+    let secondNumber = 5;
+    let expectedSum = 4;
+
+    let actualSum = divide(firstNumber, secondNumber);
+
+    expect.equal(actualSum, expectedSum);
+});
+
+
+
+
+const divideTestTwo = QUnit.test;
+
+divideTestTwo('Expect 7 and 1 to return 8', (expect) => {
+    let firstNumber = 9;
+    let secondNumber = 1;
+    let expectedSum = 9;
+
+    let actualSum = divide(firstNumber, secondNumber);
+
+    expect.equal(actualSum, expectedSum);
+});
+
+
+
